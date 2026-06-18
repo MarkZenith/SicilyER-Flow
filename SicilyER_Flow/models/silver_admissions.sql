e34402c7cc826b75f7195340c8da0f4a6c629d41
MarkZenith <governalmarcomain@gmail.com>
2026-06-18 13:47:16 +0000

    create silver layer

---
+++ SicilyER_Flow/models/silver_admissions.sql
@@ -0,0 +1,22 @@
+{{ config(
+    materialized='table'
+) }}
+
+WITH raw_data AS (
+    SELECT * FROM {{ source('er_bronze', 'raw') }}
+)
+SELECT
+    raw_data:id_accesso::VARCHAR AS id_accesso,
+    raw_data:timestamp_arrivo::TIMESTAMP AS timestamp_arrivo,
+    raw_data:id_ospedale::VARCHAR AS id_ospedale,
+    raw_data:nome_ospedale::VARCHAR AS nome_ospedale,
+    raw_data:codice_triage::VARCHAR AS codice_triage,
+    raw_data:eta_paziente::INTEGER AS eta_paziente,
+    raw_data:sesso::VARCHAR AS sesso,
+    raw_data:sintomo_principale::VARCHAR AS sintomo_principale,
+    raw_data:status::VARCHAR AS status,
+    raw_data:ora_arrivo::INTEGER AS ora_arrivo,
+    raw_data:giorno_settimana::INTEGER AS giorno_settimana,
+    raw_data:mese::INTEGER AS mese,
+    raw_data:attesa_stimata_minuti::INTEGER AS attesa_stimata_minuti,
+FROM raw_data
